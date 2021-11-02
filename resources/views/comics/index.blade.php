49b51ffb7d6bdb2e49344037913e0cdd5fd1e00d
bizoon <r.aureli@protonmail.com>
2021-11-02 16:34:33 +0000

    Index views striped-hover table

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,7 +3,35 @@
 @section('content')
 
     <div class="container">
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Title</th>
+                  <th scope="col">Description</th>
+                  <th scope="col">Price</th>
+                  <th scope="col">Series</th>
+                  <th scope="col">Type</th>
+                  <th scope="col">Sale Date</th>
+                  
+                </tr>
+              </thead>
+              @foreach ($comics as $comic)
+              <tr>
+                  <th scope="row">{{$comic["id"]}}</th>
+                  <th scope="row">{{$comic["title"]}}</th>
+                  <th scope="row">{{$comic["description"]}}</th>
+                  <th scope="row">{{$comic["price"]}}</th>
+                  <th scope="row">{{$comic["series"]}}</th>
+                  <th scope="row">{{$comic["type"]}}</th>
+                  <th scope="row">{{$comic["sale_date"]}}</th>
 
+
+              </tr>
+                  
+              @endforeach
+          </table>
     </div>
     
 @endsection
+
