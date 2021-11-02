c30eef5f96a42335fcd6537b5b00f25e6a1ee405
bizoon <r.aureli@protonmail.com>
2021-11-02 16:59:07 +0000

    Aggiunta riga azioni

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,37 +1,41 @@
 @extends('layouts.app')
-
 @section('content')
-
-    <div class="container">
-        <table class="table table-striped table-hover">
-            <thead>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <table class="table table-striped ">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Series</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Sale Date</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                @foreach ($comics as $comic)
                 <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Title</th>
-                  <th scope="col">Description</th>
-                  <th scope="col">Price</th>
-                  <th scope="col">Series</th>
-                  <th scope="col">Type</th>
-                  <th scope="col">Sale Date</th>
-                  
+                    <th scope="row">{{$comic["id"]}}</th>
+                    <th scope="row">{{$comic["title"]}}</th>
+                    <th scope="row">{{$comic["description"]}}</th>
+                    <th scope="row">{{$comic["price"]}}</th>
+                    <th scope="row">{{$comic["series"]}}</th>
+                    <th scope="row">{{$comic["type"]}}</th>
+                    <th scope="row">{{$comic["sale_date"]}}</th>
+                    <td style="display:fleinlx; ">
+                        <a><i class="far fa-eye"></i></a>
+                        <a><i class="fas fa-edit"></i></a>
+                        <a><i class="far fa-trash-alt"></i></a>
+                    </td>
                 </tr>
-              </thead>
-              @foreach ($comics as $comic)
-              <tr>
-                  <th scope="row">{{$comic["id"]}}</th>
-                  <th scope="row">{{$comic["title"]}}</th>
-                  <th scope="row">{{$comic["description"]}}</th>
-                  <th scope="row">{{$comic["price"]}}</th>
-                  <th scope="row">{{$comic["series"]}}</th>
-                  <th scope="row">{{$comic["type"]}}</th>
-                  <th scope="row">{{$comic["sale_date"]}}</th>
 
-
-              </tr>
-                  
-              @endforeach
-          </table>
+                @endforeach
+            </table>
+        </div>
     </div>
-    
-@endsection
+</div>
 
+@endsection
